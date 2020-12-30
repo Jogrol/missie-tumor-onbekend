750a6be2b51641abe02f1c55d3e454ff6a80a59f
Joey Grolleman <joeygrolleman@gmail.com>
2020-12-30 18:17:56 +0100

    Code clean up

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const { resolve } = require(`path`)
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
+exports.createPages = async ({ actions, graphql }) => {
   const {
     data: {
       allWpPage: { nodes: contentNodes },
@@ -21,27 +21,17 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   `)
 
   await Promise.all(
-    contentNodes.map(async (node, index) => {
-      const { uri, title, content, id, isFrontPage } = node
-
-      const perPage = 10
-      const page = index + 1
-      const offset = perPage * index
+    contentNodes.map(async node => {
+      const { uri, id, isFrontPage } = node
 
       const defaultPage = resolve(`./src/pages/defaultPage.js`)
       const homePage = resolve(`./src/pages/homePage.js`)
 
-      console.log("check", id, isFrontPage)
       await actions.createPage({
         component: isFrontPage ? homePage : defaultPage,
         path: `${uri}`,
         context: {
           id: id,
-          // page: page,
-          // offset: offset,
-          // totalPages: contentNodes.length,
-          // title,
-          // content,
         },
       })
     })
