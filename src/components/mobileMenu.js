e2d7f53b795cb271e9d2ee6a034f0028c840c571
Joey Grolleman <joeygrolleman@gmail.com>
2021-01-05 21:19:13 +0100

    mobile menu transition

--- src/components/mobileMenu.js
+++ src/components/mobileMenu.js
@@ -38,29 +38,28 @@ const MobileMenu = () => {
           <MenuIcon className="w-6 h-6" />
         )}
       </button>
-      {menuIsOpen && (
-        <div
-          className={"fixed bottom-0 left-0 w-screen h-screen bg-yellow-300"}
-        >
-          <div className="flex justify-center items-center w-full h-full">
-            <ul className="">
-              {data.wpMenu.menuItems.nodes.map(item => {
-                const path = item.connectedNode.node.uri ?? item.url
-                return (
-                  <li className="mb-4">
-                    <Link
-                      onClick={() => toggleMenuIsOpen(!menuIsOpen)}
-                      to={path}
-                    >
-                      <button className="text-5xl">{item.label}</button>
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+      <div
+        className={
+          menuIsOpen
+            ? "visible fixed top-0  right-0 w-screen h-screen opacity-100 bg-yellow-300 duration-200 delay-200"
+            : "invisible fixed top-0 -mt-80 opacity-0 right-0 w-screen h-screen transition-all duration-200"
+        }
+      >
+        <div className="flex justify-center items-center w-full h-full">
+          <ul className="">
+            {data.wpMenu.menuItems.nodes.map(item => {
+              const path = item.connectedNode.node.uri ?? item.url
+              return (
+                <li className="mb-8">
+                  <Link onClick={() => toggleMenuIsOpen(!menuIsOpen)} to={path}>
+                    <button className="text-5xl">{item.label}</button>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
         </div>
-      )}
+      </div>
     </>
   )
 }
