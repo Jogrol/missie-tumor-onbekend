11f14a4e7c813f04bb4ded5bca8964d61e4f6517
Joey Grolleman <joeygrolleman@gmail.com>
2021-02-10 21:28:01 +0100

    Created new pageHero

--- src/components/pageHero.js
+++ src/components/pageHero.js
@@ -1,20 +1,38 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Img from "gatsby-image"
-import StatsInformation from "../components/statsInformation"
+import scrollTo from "gatsby-plugin-smoothscroll"
+import ScrollIcon from "../assets/ScrollIcon.svg"
+import StatsInformation from "./statsInformation"
 
 const PageHero = ({ title, subtitle, description, image }) => {
   return (
-    <div className="relative">
-      <div className="absolute left-0 top-0 h-3/4 w-full z-0">
+    <div className="relative h-full sm-h-screen w-screen">
+      <div className="absolute left-0 top-0 h-full sm-h-screen min-h-screen w-screen z-0">
         <Img
           fluid={image.localFile.childImageSharp.fluid}
           alt="header"
           className="h-full w-full"
         />
       </div>
-      <div className="relative w-full h-full pt-20 z-10">
-        <StatsInformation />
+      <div className="relative w-full min-h-screen sm:h-screen pt-20 z-10 flex flex-wrap">
+        <div className="w-full sm:w-1/2 p-8 flex justify-center items-center h-full">
+          <div className="bg-gray-50 shadow-md p-8 rounded-xl w-full sm:w-2/3">
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+            <p>{description}</p>
+            <button
+              onClick={() => scrollTo("#scroll-to-donate")}
+              className="flex flex-row items-center uppercase px-4 py-3 rounded-lg border bg-yellow-500 hover:bg-yellow-300 hover:scale-150 focus:ring-2"
+            >
+              Doneer nu
+              <ScrollIcon className="animate-bounce ml-4 w-4 h-4" />
+            </button>
+          </div>
+        </div>
+        <div className="w-full h-full sm:w-1/2 flex justify-center items-center p-8">
+          <StatsInformation />
+        </div>
       </div>
     </div>
   )
--- src/components/statsInformation.js
+++ src/components/statsInformation.js
@@ -6,42 +6,24 @@ import ScrollIcon from "../assets/ScrollIcon.svg"
 
 const StatsInformation = () => {
   return (
-    <div className="container px-4 h-full">
-      <div className="flex flex-col flex-no-wrap justify-between w-full h-3/4">
-        <div className="flex-1 flex items-center">
-          <h1 className="text-4xl sm:text-8xl pt-4">Samen moet het lukken</h1>
-        </div>
-        <div className="flex-grow w-full sm:mt-8 sm:w-2/3 flex items-center">
-          <div className="flex flex-wrap justify-center sm:justify-start gap-6">
-            <div className="w-24 h-24 sm:w-52 sm:h-52">
-              <StatInfoCard />
-            </div>
-            <div className="w-24 h-24 sm:w-52 sm:h-52">
-              <StatInfoCard />
-            </div>
-            <div className="w-24 h-24 sm:w-52 sm:h-52">
-              <StatInfoCard />
-            </div>
-            <div className="w-24 h-24 sm:w-52 sm:h-52">
-              <StatInfoCard />
-            </div>
-            <div className="w-24 h-24 sm:w-52 sm:h-52">
-              <StatInfoCard />
-            </div>
-            <div className="w-24 h-24 sm:w-52 sm:h-52">
-              <StatInfoCard />
-            </div>
-          </div>
-        </div>
-        <div className="flex-1 pt-4 pb-4 sm:pb-12 sm:pt-12 flex flex-col justify-end sm:justify-center items-center">
-          <button
-            onClick={() => scrollTo("#scroll-to-donate")}
-            className="flex flex-row items-center uppercase px-4 py-3 rounded-lg border bg-yellow-500 hover:bg-yellow-300 hover:scale-150 focus:ring-2"
-          >
-            Doneer nu
-            <ScrollIcon className="animate-bounce ml-4 w-4 h-4" />
-          </button>
-        </div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+      <div className="w-24 h-24 sm:w-52 sm:h-52">
+        <StatInfoCard />
+      </div>
+      <div className="w-24 h-24 sm:w-52 sm:h-52">
+        <StatInfoCard />
+      </div>
+      <div className="w-24 h-24 sm:w-52 sm:h-52">
+        <StatInfoCard />
+      </div>
+      <div className="w-24 h-24 sm:w-52 sm:h-52">
+        <StatInfoCard />
+      </div>
+      <div className="w-24 h-24 sm:w-52 sm:h-52">
+        <StatInfoCard />
+      </div>
+      <div className="w-24 h-24 sm:w-52 sm:h-52">
+        <StatInfoCard />
       </div>
     </div>
   )
