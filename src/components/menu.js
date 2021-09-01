4e9b45247530eeeadee6a420445df48c694b4903
Joey Grolleman <joey.grolleman@incentro.com>
2021-09-01 16:43:21 +0200

    Add mobile menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,36 +1,71 @@
 import React from "react"
 import { Link } from "gatsby"
+import MenuIcon from "../assets/menuIcon.svg"
 
 const Menu = () => {
+
+
   return (
-    <div className="hidden sm:flex justify-end flex-1 items-center gap-4">
-      <Link className="btn btn-outline btn-sm" to="/">
-        Steun ons
-      </Link>
-      <div className="dropdown dropdown-end">
-        <div tabindex="0" class="btn btn-ghost rounded-btn">
-          Projecten
+    <>
+      <div className="hidden sm:flex justify-end flex-1 items-center gap-4">
+        <Link className="btn btn-outline btn-sm" to="/">
+          Steun ons
+        </Link>
+        <div className="dropdown dropdown-end">
+          <div tabindex="0" class="btn btn-ghost rounded-btn">
+            Projecten
+          </div>
+          <ul
+            tabindex="0"
+            className="p-2 shadow menu dropdown-content bg-base-100 rounded-box"
+          >
+            <li>
+              <Link to="/informatievoorziening-bewustwording">
+                Informatievoorziening
+              </Link>
+            </li>
+            <li>
+              <Link to="/ondersteunen-van-wetenschappelijk-onderzoek">
+                Wetenschappelijkonderzoek
+              </Link>
+            </li>
+            <li>
+              <Link to="/verbetering-inzet-zorg">Zorg</Link>
+            </li>
+          </ul>
         </div>
-        <ul
-          tabindex="0"
-          className="p-2 shadow menu dropdown-content bg-base-100 rounded-box"
-        >
-          <li>
-            <Link to="/informatievoorziening-bewustwording">
-              Informatievoorziening
-            </Link>
-          </li>
-          <li>
-            <Link to="/ondersteunen-van-wetenschappelijk-onderzoek">
-              Wetenschappelijkonderzoek
-            </Link>
-          </li>
-          <li>
-            <Link to="/verbetering-inzet-zorg">Zorg</Link>
-          </li>
-        </ul>
       </div>
-    </div>
+      <div className="flex justify-end flex-1 sm:hidden">
+        <div className="dropdown dropdown-end">
+          <div tabindex="0" className="h-full flex justify-center items-center">
+           <MenuIcon className="h-8 w-8 text-blue-600" />
+          </div>
+          <ul
+            tabindex="0"
+            className="p-2 shadow menu dropdown-content bg-base-100 rounded-box"
+          >
+            <li>
+              <Link to="/">
+                Steun ons
+              </Link>
+            </li>
+            <li>
+              <Link to="/informatievoorziening-bewustwording">
+                Informatievoorziening
+              </Link>
+            </li>
+            <li>
+              <Link to="/ondersteunen-van-wetenschappelijk-onderzoek">
+                Wetenschappelijkonderzoek
+              </Link>
+            </li>
+            <li>
+              <Link to="/verbetering-inzet-zorg">Zorg</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
