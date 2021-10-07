44ab0a80d4247291fd77bbe755b6b89ab03941ca
Joey Grolleman <joey.grolleman@incentro.com>
2021-10-07 17:26:19 +0200

    Fix styling issues pageHeroVideoComponent

--- src/components/pageHeroWithVideo.tsx
+++ src/components/pageHeroWithVideo.tsx
@@ -18,42 +18,43 @@ const PageHeroWithVideo = () => {
         playsInline
         autoPlay
         muted={isMuted}
-        className="hero-overlay object-cover h-full w-full"
+        className="hero-overlay object-cover h-screen w-full"
       >
         <source src={BackgroundVideo} type="video/mp4" />
       </video>
-      <div className="relative hero-content h-5/6 w-full z-20">
-        <div className="absolute top-12 right-12">
-          <button onClick={() => setIsmuted(!isMuted)}>
-            {isMuted ? (
-              <VolumeIcon className="h-8 w-8 animate-pulse" />
-            ) : (
-              <MuteIcon className="h-8 w-8 animate-pulse" />
-            )}
-          </button>
-        </div>
-
-        <div className="flex flex-col items-center">
-          <div className="relative">
-            <p className="absolute text-white text-2xl sm:text-5xl text-center leading-normal text-center p-4 animate-in">
-              Elk jaar worden 1500 Nederlanders slachtoffer van een tumor die
-              nooit wordt gevonden.
-            </p>
-            <p className="text-white text-2xl sm:text-5xl text-center leading-normal opacity-0 p-4 animate-out">
-              Steun ons in de strijd tegen PTO, <br /> Primaire Tumor Onbekend
-            </p>
+      <div className="relative px-4 pt-6 sm:pb- h-5/6 w-full z-20">
+        <div className="h-full container flex flex-col justify-between py-2 sm:py-10">
+          <div className="flex justify-end">
+            <button onClick={() => setIsmuted(!isMuted)}>
+              {isMuted ? (
+                <VolumeIcon className="h-8 w-8 animate-pulse" />
+              ) : (
+                <MuteIcon className="h-8 w-8 animate-pulse" />
+              )}
+            </button>
           </div>
-        </div>
-        <div className="absolute bottom-0 left-0 flex flex-col sm:flex-row sm:justify-center w-full px-4 pb-4 sm:pb-24 gap-4 sm:gap-16">
-          <button
-            className="btn sm:btn-lg btn-primary w-full sm:w-96"
-            onClick={handleDonateNowClick}
-          >
-            Doneer nu
-          </button>
-          <a className="btn sm:btn-lg btn-secondary w-full sm:w-96" href="#">
-            Deel nu jouw verhaal met PTO
-          </a>
+          <div>
+            <div className="relative">
+              <p className="absolute text-white text-2xl sm:text-5xl text-center leading-normal text-center p-4 animate-in">
+                Elk jaar worden 1500 Nederlanders slachtoffer van een tumor die
+                nooit wordt gevonden.
+              </p>
+              <p className="text-white text-2xl sm:text-5xl text-center leading-normal opacity-0 p-4 animate-out">
+                Steun ons in de strijd tegen PTO, <br /> Primaire Tumor Onbekend
+              </p>
+            </div>
+          </div>
+          <div className="flex w-full flex-col sm:flex-row flex-wrap sm:justify-center md:flex-row gap-4 sm:gap-6">
+            <button
+              className="btn btn-primary w-full sm:w-96"
+              onClick={handleDonateNowClick}
+            >
+              Doneer nu
+            </button>
+            <a className="btn btn-secondary w-full sm:w-96" href="#">
+              Deel nu jouw verhaal met PTO
+            </a>
+          </div>
         </div>
       </div>
     </div>
