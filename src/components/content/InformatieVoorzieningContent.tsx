02341407661638b6b7900bf3a2d29c50146e868f
joey-nrc <j.grolleman@nrc.nl>
2025-03-03 20:38:39 +0100

    Update content and goals in InformatieVoorzieningContent

--- src/components/content/InformatieVoorzieningContent.tsx
+++ src/components/content/InformatieVoorzieningContent.tsx
@@ -1,63 +1,63 @@
-import React from 'react';
+import React from "react"
 
 const InformatieVoorzieningContent: React.FC = () => {
   return (
     <div className="col-span-4 sm:col-span-2">
       <h3>
-        <strong>Doel: €25.000</strong>
+        <strong>Doel: €11.000</strong>
       </h3>
 
       <p>
-        Primaire tumor onbekend is een relatief onbekende
-        kankerdiagnose, juist omdat er geen complete diagnose gegeven
-        kan worden. Echter hebben deze mensen wel te maken met
-        uitgezaaide kanker. Missie Tumor Onbekend wil graag dat alle
-        patiënten, naasten, zorgverleners en onderzoekers goed op de
-        hoogte blijven van de laatste kennis en&nbsp; vooruitgang, en
-        ontwikkelt materialen ten behoeve hiervan. Daarnaast werkt
-        Missie Tumor Onbekend ook aan bredere maatschappelijke awareness
-        en bewustwording, zodat PTO-patiënten zich meer gesteund voelen.
-        Meer bekendheid creëert ook nieuwe mogelijkheden om meer
-        projecten voor PTO-patiënten te realiseren.
+        Primaire tumor onbekend is een relatief onbekende kankerdiagnose, juist
+        omdat er geen complete diagnose gegeven kan worden. Echter hebben deze
+        mensen wel te maken met uitgezaaide kanker. Missie Tumor Onbekend wil
+        graag dat alle patiënten, naasten, zorgverleners en onderzoekers goed op
+        de hoogte blijven van de laatste kennis en  vooruitgang, en ontwikkelt
+        materialen ten behoeve hiervan. Daarnaast werkt Missie Tumor Onbekend
+        ook aan bredere maatschappelijke awareness en bewustwording, zodat
+        PTO-patiënten zich meer gesteund voelen. Meer bekendheid creëert ook
+        nieuwe mogelijkheden om meer projecten voor PTO-patiënten te realiseren.
       </p>
 
       <p>
         <strong>
           Missie Tumor Onbekend wil onder andere de volgende projecten
-          realiseren in het kader van informatievoorziening &amp;
-          bewustwording.
+          realiseren in het kader van informatievoorziening &amp; bewustwording
         </strong>
       </p>
 
       <ul>
         <li>
-          Meer verspreiding van de
-          <a
-            href="/static/1aae3e7b2fba425c574c27affa8dc961/PZK-folder-PTO_online.pdf"
-            data-type="URL"
-            data-id="/static/1aae3e7b2fba425c574c27affa8dc961/PZK-folder-PTO_online.pdf"
-          >
-            PTO informatiefolder
-          </a>
-          voor patiënten en naasten
+          Ontwikkelen van nieuwe informatiefilmpjes- en materialen naar
+          aanleiding van de ge-update klinische richtlijn (de richtlijn wordt
+          eind 2025 verwacht)
         </li>
         <li>
-          Organisatie van het eerste landelijke PTO symposium voor
-          zorgverleners, patiënten en wetenschappers. Het symposium zal
-          in samenwerking met
+          Organisatie van een landelijk (twee)jaarlijks wetenschappelijk
+          symposium voor zorgverleners, wetenschappers en patiënten. Het
+          symposium zal in samenwerking met{" "}
           <a
-            href="http://cupp-nl.eu/"
+            href="https://cupp-nl.eu/"
             data-type="URL"
-            data-id="http://cupp-nl.eu/"
+            data-id="https://cupp-nl.eu/"
           >
             CUPP-NL
-          </a>
-          georganiseerd worden
+          </a>{" "}
+          georganiseerd worden (eind 2025/begin 2026 naar verwachting een
+          volgend symposium)
         </li>
-        <li>Extra inzet van de PTO awareness campagne </li>
+        <li>
+          Onderhoud van en verdere verspreiding van de PTO-informatiefolder voor
+          patiënten en naasten
+        </li>
+        <li>
+          Onderhoud van de PTO-website &amp; nieuwsbrief met informatie voor
+          patiënten en naasten
+        </li>
+        <li>Internationale samenwerking</li>
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default InformatieVoorzieningContent;
+export default InformatieVoorzieningContent
