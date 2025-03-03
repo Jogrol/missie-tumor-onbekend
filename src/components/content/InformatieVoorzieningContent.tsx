014aa5eb74456eef863a48ca7b96766b0e4af902
joey-nrc <j.grolleman@nrc.nl>
2025-03-03 20:33:17 +0100

    Add content components for informational, care, and research goals

---
+++ src/components/content/InformatieVoorzieningContent.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const InformatieVoorzieningContent: React.FC = () => {
+  return (
+    <div className="col-span-4 sm:col-span-2">
+      <h3>
+        <strong>Doel: €25.000</strong>
+      </h3>
+
+      <p>
+        Primaire tumor onbekend is een relatief onbekende
+        kankerdiagnose, juist omdat er geen complete diagnose gegeven
+        kan worden. Echter hebben deze mensen wel te maken met
+        uitgezaaide kanker. Missie Tumor Onbekend wil graag dat alle
+        patiënten, naasten, zorgverleners en onderzoekers goed op de
+        hoogte blijven van de laatste kennis en&nbsp; vooruitgang, en
+        ontwikkelt materialen ten behoeve hiervan. Daarnaast werkt
+        Missie Tumor Onbekend ook aan bredere maatschappelijke awareness
+        en bewustwording, zodat PTO-patiënten zich meer gesteund voelen.
+        Meer bekendheid creëert ook nieuwe mogelijkheden om meer
+        projecten voor PTO-patiënten te realiseren.
+      </p>
+
+      <p>
+        <strong>
+          Missie Tumor Onbekend wil onder andere de volgende projecten
+          realiseren in het kader van informatievoorziening &amp;
+          bewustwording.
+        </strong>
+      </p>
+
+      <ul>
+        <li>
+          Meer verspreiding van de
+          <a
+            href="/static/1aae3e7b2fba425c574c27affa8dc961/PZK-folder-PTO_online.pdf"
+            data-type="URL"
+            data-id="/static/1aae3e7b2fba425c574c27affa8dc961/PZK-folder-PTO_online.pdf"
+          >
+            PTO informatiefolder
+          </a>
+          voor patiënten en naasten
+        </li>
+        <li>
+          Organisatie van het eerste landelijke PTO symposium voor
+          zorgverleners, patiënten en wetenschappers. Het symposium zal
+          in samenwerking met
+          <a
+            href="http://cupp-nl.eu/"
+            data-type="URL"
+            data-id="http://cupp-nl.eu/"
+          >
+            CUPP-NL
+          </a>
+          georganiseerd worden
+        </li>
+        <li>Extra inzet van de PTO awareness campagne </li>
+      </ul>
+    </div>
+  );
+};
+
+export default InformatieVoorzieningContent;
---
+++ src/components/content/VerbeteringZorgContent.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const VerbeteringZorgContent: React.FC = () => {
+  return (
+    <div className="col-span-4 sm:col-span-2">
+      <h3>
+        <strong>Doel: €75.000</strong>
+      </h3>
+
+      <p>
+        Missie Tumor Onbekend werkt samen met zorgverleners hard aan het vergroten van de toegang tot diagnostiek, tumorgerichte behandelingen, palliatieve zorg en psychosociale steun voor alle PTO-patiënten. Zo is Missie Tumor Onbekend actief betrokken bij de inrichting van het landelijke zorgpad voor PTO, de vernieuwing van de landelijke klinische richtlijn, en steunt projecten die de implementatie en inzet hiervan vergroten.
+      </p>
+
+      <p>
+        <strong>
+          Missie Tumor Onbekend wil onder andere de volgende projecten realiseren in het kader van het verbeteren van de inzet van zorg voor PTO-patiënten.
+        </strong>
+      </p>
+
+      <ul>
+        <li>Ontwikkeling en uitvoer van een patient journey mapping voor PTO</li>
+        <li>Ontwikkeling van een e-learning voor verpleegkundigen over de tijdige inzet van palliatieve zorg bij patiënten met een relatief korte levensverwachting</li>
+        <li>Ondersteunen van naasten en nabestaanden die met onzekerheid en/of complexe rouw te maken hebben</li>
+      </ul>
+
+      <p></p>
+    </div>
+  );
+};
+
+export default VerbeteringZorgContent;
