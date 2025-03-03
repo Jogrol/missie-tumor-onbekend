848817ec57c93755763eca8b59ce0be6cb4b5013
joey-nrc <j.grolleman@nrc.nl>
2025-03-03 06:22:48 +0100

    fix: content in Ondersteunen van wetenschappelijk onderzoek

--- src/pages/projectPage.tsx
+++ src/pages/projectPage.tsx
@@ -56,10 +56,17 @@ const ProjectPage = ({ data }: ProjectPageDataModel): JSX.Element => {
         )}
         {projectProgress && <ProgressBar {...projectProgress} />}
         <div className="mt-4">
-          <div
-            className="col-span-4 sm:col-span-2"
-            dangerouslySetInnerHTML={{ __html: pageContent }}
-          />
+          {pageTitle === "Ondersteunen van wetenschappelijk onderzoek" && (
+            <div className="col-span-4 sm:col-span-2">
+              <h3><strong>Doel: €100.000</strong></h3>
+              <p>Missie Tumor Onbekend steunt wetenschappers en zorgverleners graag in relevante onderzoeken naar betere diagnostiek, geneesmiddelen, tumorgerichte behandelingen en palliatieve zorg voor een onbekende primaire tumor. Door nieuwe inzichten kan de schrijnende lage overleving van een PTO verbeteren en de kwaliteit van leven voor de patiënt vooruit. Daarnaast neemt Missie Tumor Onbekend ook zelf het voortouw in het bijeenbrengen van coalities om gezamenlijk wetenschappelijk onderzoek uit te voeren dat in het belang van de PTO-patiënt is. Bij het beoordelen van wetenschappelijk onderzoeksvoorstellen krijgt Missie Tumor Onbekend steun van <a href="http://cupp-nl.eu/" data-type="URL" data-id="http://cupp-nl.eu/">CUPP-NL</a>, het landelijke expertplatform over PTO.</p>
+              <p><strong>Missie Tumor Onbekend wil onder andere de volgende projecten realiseren in het kader van het ondersteunen van wetenschappelijk onderzoek.</strong></p>
+              <ul>
+                <li>Extra ondersteuning voor FAPI-PET studie (ingediend bij KWF, in afwachting van besluit), voor meer informatie kunt u terecht bij: <a href="http://cupp-nl.eu/projects/18f-f-fapi-petct-to-identify-carcinoma-of-hitherto-unknown-primary-origin-improving-minimally-invasive-cancer-diagnostics/" data-type="URL" data-id="http://cupp-nl.eu/projects/18f-f-fapi-petct-to-identify-carcinoma-of-hitherto-unknown-primary-origin-improving-minimally-invasive-cancer-diagnostics/">[18F]F-FAPI PET/CT to Identify Carcinoma of hitherto Unknown Primary origin: improving minimally-invasive cancer diagnostics. | CUPP NL </a></li>
+                <li>Het ondersteunen van de landelijke database van klinische geanonimiseerde data van PTO- patiënten om zo de impact van het PTO-zorgpad in kaart te brengen en ook toekomstig wetenschappelijk onderzoek te faciliteren</li>
+              </ul>
+            </div>
+          )}
         </div>
       </PageSection>
       <div id="scroll-to-donate">
